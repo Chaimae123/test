5455d77cf7af3822ae95c814aa04ada8019aba5f
Chaimae123 <chaimae@mbp-de-mac-34.home>
2017-08-01 12:26:37 +0100

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by chaimae aklibous on 01/08/2017.
+//  Copyright © 2017 chaimae aklibous. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
